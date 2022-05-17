eff9ac754298de5f8f9dd75c20fe053b0012cf83
Mike Kudzin <mike.kudzin@gmail.com>
2022-05-17 16:07:55 +0200

    Added transactional fetch for POJO with relations

--- feature-match/src/main/java/com/mk/match/data/MatchesDAO.kt
+++ feature-match/src/main/java/com/mk/match/data/MatchesDAO.kt
@@ -1,9 +1,6 @@
 package com.mk.match.data
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Maybe
@@ -17,11 +14,13 @@ interface MatchesDAO : MatchesRepository{
     @Update
     override fun updateMatch(match: MatchEntity): Completable
 
-    @Query("SELECT * FROM ${MatchEntity.MATCH_TABLE_NAME} ORDER BY recordTime DESC")
-    override fun getAllMatches(): Flowable<List<MatchWithPlayers>>
-
+    @Transaction
     @Query("SELECT * FROM ${MatchEntity.MATCH_TABLE_NAME} WHERE id = :matchId")
     override fun getMatch(matchId: Int): Maybe<MatchWithPlayers>
+
+    @Transaction
+    @Query("SELECT * FROM ${MatchEntity.MATCH_TABLE_NAME} ORDER BY recordTime DESC")
+    override fun getAllMatches(): Flowable<List<MatchWithPlayers>>
 
     @Query("DELETE FROM ${MatchEntity.MATCH_TABLE_NAME} WHERE id = :matchId")
     override fun deleteMatch(matchId: Int) : Completable
