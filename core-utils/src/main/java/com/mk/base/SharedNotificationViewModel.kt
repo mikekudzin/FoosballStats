13473163fd665b04a092d89dd73ffec5fe48987f
Mike Kudzin <mike.kudzin@gmail.com>
2022-05-17 16:23:58 +0200

    Disclaimer for SharedNotificationViewModel

--- core-utils/src/main/java/com/mk/base/SharedNotificationViewModel.kt
+++ core-utils/src/main/java/com/mk/base/SharedNotificationViewModel.kt
@@ -3,6 +3,14 @@ package com.mk.base
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 
+/*
+ * It is a pattern of how we can share data between a number of components (Fragment-Fragment,
+ * Fragment-Activity) by using Jetpack lifecycle aware components.
+ *
+ * Although the notification use case is not perfect from the architectural point of view by making
+ * modules fragments aware of some aspects of an activity implementation, it's main goal to demonstrate
+ * the general approach.
+ */
 class SharedNotificationViewModel: ViewModel() {
 
     private val _error = SingleLiveEvent<String>()
